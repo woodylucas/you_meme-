34937fa3f106625cdc2a5597a1e2be160fd27109
woodylucas <wlucas0311@gmail.com>
2019-01-22 19:13:59 -0500

    edit schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_22_185059) do
+ActiveRecord::Schema.define(version: 2019_01_23_001126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 2019_01_22_185059) do
     t.integer "meme_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "likes"
   end
 
   create_table "memes", force: :cascade do |t|
