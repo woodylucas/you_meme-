1a50611f6d1383cc2ae72823bbfd5bef7f7e14bc
jeff-gosselin <jgoz.email@gmail.com>
2019-01-25 12:25:49 -0500

    take a look

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   skip_before_action :authorized, only: %i[new create]
-  before_action :set_user, only: %i[show edit]
+  before_action :set_user, only: %i[show edit update]
 
   def new
     @user = User.new
@@ -16,7 +16,13 @@ class UsersController < ApplicationController
   end
 
   def edit
-  end 
+  end
+
+	def update
+		@user.update(user_params)
+		redirect_to user_path(@user)
+	end
+
 
   def set_user
     @user = User.find(params[:id])
@@ -24,6 +30,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:username, :bio, :password)
+    params.require(:user).permit(:username, :bio, :password, :profile_pic)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users, only: %i[show new create edit]
+  resources :users, only: %i[show new create edit update]
   resources :memes, only: %i[index new show create edit update]
 	delete '/memes/:id', to: 'memes#destroy'
   resources :likes, only: %i[]
